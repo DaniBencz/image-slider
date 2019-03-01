15d940dfa677658e55eb8205747005ed8a988e7e
Daniel Bencz <daniel.bencz@gmail.com>
2019-03-01 17:41:50 +0100

    went on with project

--- slider.js
+++ slider.js
@@ -1,10 +1,20 @@
 'use strict';
 
-let button = document.querySelectorAll('.icon');
-button.forEach(icon => {
+let icon = document.querySelectorAll('.icon');
+icon.forEach(icon => {
   icon.onclick = () => {
     let img = document.querySelector('.main-image');
-    img.setAttribute('src',`${icon.getAttribute('src')}`);
+    img.setAttribute('src', `${icon.getAttribute('src')}`);
   };
+});
 
-})
+let rightButton = document.querySelector('.right');
+rightButton.onclick = () => {
+  alert('right');
+};
+
+let leftButton = document.querySelector('.left');
+leftButton.onclick = () => {
+  alert('left');
+};
+
