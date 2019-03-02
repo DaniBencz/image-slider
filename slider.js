c8079b75dc2e397bcd80a2896bc3f737c7997f4e
Daniel Bencz <daniel.bencz@gmail.com>
2019-03-02 15:56:06 +0100

    elaborated on-click

--- slider.js
+++ slider.js
@@ -1,19 +1,21 @@
 'use strict';
 
 //thumnails onclick
+let index = 0;
 let icon = document.querySelectorAll('.icon');
-icon.forEach(icon => {
+icon.forEach((icon, i) => {
   icon.onclick = () => {
+    index=i;
+    console.log(index);
     let img = document.querySelector('.main-image');
     img.setAttribute('src', `${icon.getAttribute('src')}`);
   };
 });
 
 //right button
-let index = 0;
 let rightButton = document.querySelector('.right');
 rightButton.onclick = () => {
-  if (icon.length-1 > index) {
+  if (icon.length - 1 > index) {
     index++;
     console.log(index);
   }
@@ -25,8 +27,8 @@ rightButton.onclick = () => {
 let leftButton = document.querySelector('.left');
 leftButton.onclick = () => {
   if (index > 0) {
-  index--;
-  console.log(index);
+    index--;
+    console.log(index);
   }
   let img = document.querySelector('.main-image');
   img.setAttribute('src', `${icon[index].getAttribute('src')}`);
